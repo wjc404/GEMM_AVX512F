d9f1d2a77a93183a2df91599c0f796a3b0fbbe2e
wjc404 <52632443+wjc404@users.noreply.github.com>
2019-10-03 02:02:00 +0800

    Add files via upload

--- dgemm_tune.h
+++ dgemm_tune.h
@@ -1,10 +1,12 @@
 // The size of packed A matrix is 6kB * GEMM_LOOP_TIMES_K, which should not succeeds the capacity of L1 cache.
 // The value of PREF_CYCLES_PACKED_A should be greater than the latency of L2 cache (in cycles).
 // The value of PREF_CYCLES_PACKED_B should be greater than the latency of L3 cache (in cycles).
+// GEMM_NUM_ACCUMULATORS: 12 or 24
 
-# define GEMM_UNROLL_M_VEC 6
-# define GEMM_LOOP_TIMES_N 32
-# define GEMM_LOOP_TIMES_K 6
+# define GEMM_UNROLL_M_VEC 2
+# define GEMM_NUM_ACCUMULATORS 12
+# define GEMM_LOOP_TIMES_N 16
+# define GEMM_LOOP_TIMES_K 4
 # define PREF_CYCLES_PACKED_A 12
 # define PREF_CYCLES_PACKED_B 36
 
