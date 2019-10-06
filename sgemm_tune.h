083ddce59abced0abca6424786b58b2df2e96bb6
wjc404 <52632443+wjc404@users.noreply.github.com>
2019-10-06 09:46:19 +0800

    Update sgemm_tune.h

--- sgemm_tune.h
+++ sgemm_tune.h
@@ -1,5 +1,5 @@
 // The size of packed A matrix is 256B * GEMM_LOOP_TIMES_K * GEMM_UNROLL_N * GEMM_UNROL_M_VEC, which should not exceed the capacity of L1 cache.
-// The size of packed B matrix(should be a multiple of 64B) is 16B * (GEMM_UNROLL_N^2) * GEMM_LOOP_TIMES_N * GEMM_LOOP_TIMES_K * GEMM_UNROLL_M_VEC, which should not exceed the capacity of L3 cache per core.
+// The size of packed B matrix(should be a multiple of 64B) is 32B * (GEMM_UNROLL_N^2) * GEMM_LOOP_TIMES_N * GEMM_LOOP_TIMES_K (ifndef NO_REPEAT_C_BLOCK: * GEMM_UNROLL_M_VEC), which should not exceed the capacity of L3 cache per core.
 // GEMM_UNROLL_M_VEC * GEMM_UNROLL_N should be no less than CPU_NUM_512FMA_UNITS * "latency of fma instructions in cycles".
 // The value of PREF_CYCLES_PACKED_A should be greater than the latency of L2 cache (in cycles).
 // The value of PREF_CYCLES_PACKED_B should be greater than the latency of L3 cache (in cycles).
