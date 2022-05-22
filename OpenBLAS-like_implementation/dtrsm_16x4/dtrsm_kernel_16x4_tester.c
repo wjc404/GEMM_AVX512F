23a2a8929a48f8ce7df35e30ac4b98dac184ab50
wjc404 <52632443+wjc404@users.noreply.github.com>
2022-05-23 00:04:48 +0800

    Add files via upload

---
+++ OpenBLAS-like_implementation/dtrsm_16x4/dtrsm_kernel_16x4_tester.c
@@ -0,0 +1,366 @@
+typedef long BLASLONG;
+
+extern void dtrsm_solve_ln_m1n1(double *sa, double *sb, double *C, BLASLONG LDB, BLASLONG LDC);
+extern void dtrsm_solve_ln_m2n1(double *sa, double *sb, double *C, BLASLONG LDB, BLASLONG LDC);
+extern void dtrsm_solve_lt_m1n1(double *sa, double *sb, double *C, BLASLONG LDB, BLASLONG LDC);
+extern void dtrsm_solve_lt_m2n1(double *sa, double *sb, double *C, BLASLONG LDB, BLASLONG LDC);
+extern void dtrsm_solve_ln_m1n2(double *sa, double *sb, double *C, BLASLONG LDB, BLASLONG LDC);
+extern void dtrsm_solve_ln_m2n2(double *sa, double *sb, double *C, BLASLONG LDB, BLASLONG LDC);
+extern void dtrsm_solve_lt_m1n2(double *sa, double *sb, double *C, BLASLONG LDB, BLASLONG LDC);
+extern void dtrsm_solve_lt_m2n2(double *sa, double *sb, double *C, BLASLONG LDB, BLASLONG LDC);
+extern void dtrsm_solve_ln_m4n1(double *sa, double *sb, double *C, BLASLONG LDB, BLASLONG LDC);
+extern void dtrsm_solve_ln_m8n1(double *sa, double *sb, double *C, BLASLONG LDB, BLASLONG LDC);
+extern void dtrsm_solve_ln_m16n1(double *sa, double *sb, double *C, BLASLONG LDB, BLASLONG LDC);
+extern void dtrsm_solve_lt_m4n1(double *sa, double *sb, double *C, BLASLONG LDB, BLASLONG LDC);
+extern void dtrsm_solve_lt_m8n1(double *sa, double *sb, double *C, BLASLONG LDB, BLASLONG LDC);
+extern void dtrsm_solve_lt_m16n1(double *sa, double *sb, double *C, BLASLONG LDB, BLASLONG LDC);
+extern void dtrsm_solve_ln_m4n2(double *sa, double *sb, double *C, BLASLONG LDB, BLASLONG LDC);
+extern void dtrsm_solve_ln_m8n2(double *sa, double *sb, double *C, BLASLONG LDB, BLASLONG LDC);
+extern void dtrsm_solve_ln_m16n2(double *sa, double *sb, double *C, BLASLONG LDB, BLASLONG LDC);
+extern void dtrsm_solve_lt_m4n2(double *sa, double *sb, double *C, BLASLONG LDB, BLASLONG LDC);
+extern void dtrsm_solve_lt_m8n2(double *sa, double *sb, double *C, BLASLONG LDB, BLASLONG LDC);
+extern void dtrsm_solve_lt_m16n2(double *sa, double *sb, double *C, BLASLONG LDB, BLASLONG LDC);
+extern void dtrsm_solve_ln_m1n4(double *sa, double *sb, double *C, BLASLONG LDB, BLASLONG LDC);
+extern void dtrsm_solve_lt_m1n4(double *sa, double *sb, double *C, BLASLONG LDB, BLASLONG LDC);
+extern void dtrsm_solve_ln_m1n8(double *sa, double *sb, double *C, BLASLONG LDB, BLASLONG LDC);
+extern void dtrsm_solve_lt_m1n8(double *sa, double *sb, double *C, BLASLONG LDB, BLASLONG LDC);
+extern void dtrsm_solve_ln_m1n12(double *sa, double *sb, double *C, BLASLONG LDB, BLASLONG LDC);
+extern void dtrsm_solve_lt_m1n12(double *sa, double *sb, double *C, BLASLONG LDB, BLASLONG LDC);
+extern void dtrsm_solve_ln_m2n4(double *sa, double *sb, double *C, BLASLONG LDB, BLASLONG LDC);
+extern void dtrsm_solve_lt_m2n4(double *sa, double *sb, double *C, BLASLONG LDB, BLASLONG LDC);
+extern void dtrsm_solve_ln_m2n8(double *sa, double *sb, double *C, BLASLONG LDB, BLASLONG LDC);
+extern void dtrsm_solve_lt_m2n8(double *sa, double *sb, double *C, BLASLONG LDB, BLASLONG LDC);
+extern void dtrsm_solve_ln_m2n12(double *sa, double *sb, double *C, BLASLONG LDB, BLASLONG LDC);
+extern void dtrsm_solve_lt_m2n12(double *sa, double *sb, double *C, BLASLONG LDB, BLASLONG LDC);
+extern void dtrsm_solve_ln_m4n4(double *sa, double *sb, double *C, BLASLONG LDB, BLASLONG LDC);
+extern void dtrsm_solve_lt_m4n4(double *sa, double *sb, double *C, BLASLONG LDB, BLASLONG LDC);
+extern void dtrsm_solve_ln_m4n8(double *sa, double *sb, double *C, BLASLONG LDB, BLASLONG LDC);
+extern void dtrsm_solve_lt_m4n8(double *sa, double *sb, double *C, BLASLONG LDB, BLASLONG LDC);
+extern void dtrsm_solve_ln_m4n12(double *sa, double *sb, double *C, BLASLONG LDB, BLASLONG LDC);
+extern void dtrsm_solve_lt_m4n12(double *sa, double *sb, double *C, BLASLONG LDB, BLASLONG LDC);
+extern void dtrsm_solve_ln_m8n4(double *sa, double *sb, double *C, BLASLONG LDB, BLASLONG LDC);
+extern void dtrsm_solve_lt_m8n4(double *sa, double *sb, double *C, BLASLONG LDB, BLASLONG LDC);
+extern void dtrsm_solve_ln_m8n8(double *sa, double *sb, double *C, BLASLONG LDB, BLASLONG LDC);
+extern void dtrsm_solve_lt_m8n8(double *sa, double *sb, double *C, BLASLONG LDB, BLASLONG LDC);
+extern void dtrsm_solve_ln_m8n12(double *sa, double *sb, double *C, BLASLONG LDB, BLASLONG LDC);
+extern void dtrsm_solve_lt_m8n12(double *sa, double *sb, double *C, BLASLONG LDB, BLASLONG LDC);
+extern void dtrsm_solve_ln_m16n4(double *sa, double *sb, double *C, BLASLONG LDB, BLASLONG LDC);
+extern void dtrsm_solve_lt_m16n4(double *sa, double *sb, double *C, BLASLONG LDB, BLASLONG LDC);
+extern void dtrsm_solve_ln_m16n8(double *sa, double *sb, double *C, BLASLONG LDB, BLASLONG LDC);
+extern void dtrsm_solve_lt_m16n8(double *sa, double *sb, double *C, BLASLONG LDB, BLASLONG LDC);
+extern void dtrsm_solve_ln_m16n12(double *sa, double *sb, double *C, BLASLONG LDB, BLASLONG LDC);
+extern void dtrsm_solve_lt_m16n12(double *sa, double *sb, double *C, BLASLONG LDB, BLASLONG LDC);
+extern void dtrsm_solve_rn_m8n4(double *sa, double *sb, double *C, BLASLONG LDB, BLASLONG LDC);
+extern void dtrsm_solve_rt_m8n4(double *sa, double *sb, double *C, BLASLONG LDB, BLASLONG LDC);
+extern void dtrsm_solve_rn_m8n8(double *sa, double *sb, double *C, BLASLONG LDB, BLASLONG LDC);
+extern void dtrsm_solve_rt_m8n8(double *sa, double *sb, double *C, BLASLONG LDB, BLASLONG LDC);
+extern void dtrsm_solve_rn_m8n12(double *sa, double *sb, double *C, BLASLONG LDB, BLASLONG LDC);
+extern void dtrsm_solve_rt_m8n12(double *sa, double *sb, double *C, BLASLONG LDB, BLASLONG LDC);
+extern void dtrsm_solve_rn_m16n4(double *sa, double *sb, double *C, BLASLONG LDB, BLASLONG LDC);
+extern void dtrsm_solve_rt_m16n4(double *sa, double *sb, double *C, BLASLONG LDB, BLASLONG LDC);
+extern void dtrsm_solve_rn_m16n8(double *sa, double *sb, double *C, BLASLONG LDB, BLASLONG LDC);
+extern void dtrsm_solve_rt_m16n8(double *sa, double *sb, double *C, BLASLONG LDB, BLASLONG LDC);
+extern void dtrsm_solve_rn_m16n12(double *sa, double *sb, double *C, BLASLONG LDB, BLASLONG LDC);
+extern void dtrsm_solve_rt_m16n12(double *sa, double *sb, double *C, BLASLONG LDB, BLASLONG LDC);
+
+static inline void update_c(const BLASLONG M, const BLASLONG N,
+  const double *sb, double *C, BLASLONG LDC) {
+
+  for (BLASLONG n = 0; n < N; ++n) {
+    double *c_ = C + n * LDC;
+    const double *b_ = sb + n;
+    for (BLASLONG m = 0; m < M; ++m) {
+      c_[m] += (*b_);
+      b_ += N;
+    }
+  }
+}
+
+static inline void update_c_a(const BLASLONG M, const BLASLONG N,
+  const double *sa, double *C, BLASLONG LDC) {
+
+  for (BLASLONG n = 0; n < N; ++n) {
+    double *c_ = C + n * LDC;
+    const double *a_ = sa + n * M;
+    for (BLASLONG m = 0; m < M; ++m) {
+      c_[m] += a_[m];
+    }
+  }
+}
+
+static inline const double *get_b_ref(const double *base,
+  unsigned long ldb, unsigned char unroll, unsigned long k, unsigned long n) {
+
+  return base + (n >> 2) * (ldb << 2) + k * unroll + (n & 3u);
+}
+
+static void solve_rn_reference(const BLASLONG M, const BLASLONG N,
+  double *sa, double *sb, double *C, BLASLONG LDB, BLASLONG LDC) {
+
+  update_c_a(M, N, sa, C, LDC);
+  const unsigned char unroll_n = N > 4 ? 4 : N;
+
+  for (BLASLONG nk = 0; nk < N; ++nk) {
+    double b0 = *get_b_ref(sb, LDB, unroll_n, nk, nk);
+    double *c_ = C + nk * LDC;
+    double *a_ = sa + nk * M;
+    for (BLASLONG m = 0; m < M; ++m) {
+      c_[m] *= b0;
+      a_[m] = c_[m];
+    }
+    for (BLASLONG n_ri = nk + 1; n_ri < N; ++n_ri) {
+      double b1 = *get_b_ref(sb, LDB, unroll_n, nk, n_ri);
+      c_ = C + n_ri * LDC;
+      for (BLASLONG m = 0; m < M; ++m) {
+        c_[m] -= a_[m] * b1;
+      }
+    }
+  }
+}
+
+static void solve_rt_reference(const BLASLONG M, const BLASLONG N,
+  double *sa, double *sb, double *C, BLASLONG LDB, BLASLONG LDC) {
+
+  update_c_a(M, N, sa, C, LDC);
+  const unsigned char unroll_n = N > 4 ? 4 : N;
+
+  for (BLASLONG nk = N - 1; nk >= 0; nk--) {
+    double b0 = *get_b_ref(sb, LDB, unroll_n, nk, nk);
+    double *c_ = C + nk * LDC;
+    double *a_ = sa + nk * M;
+    for (BLASLONG m = 0; m < M; ++m) {
+      c_[m] *= b0;
+      a_[m] = c_[m];
+    }
+    for (BLASLONG n_le = 0; n_le < nk; ++n_le) {
+      double b1 = *get_b_ref(sb, LDB, unroll_n, nk, n_le);
+      c_ = C + n_le * LDC;
+      for (BLASLONG m = 0; m < M; ++m) {
+        c_[m] -= a_[m] * b1;
+      }
+    }
+  }
+}
+
+static inline void solve_ln_ref(const BLASLONG M, const BLASLONG N,
+  const double *sa, double *sb, double *C, BLASLONG LDC) {
+
+  for (BLASLONG mk = M - 1; mk >= 0; mk--) {
+    double a0 = sa[mk * M + mk];
+    for (BLASLONG n = 0; n < N; ++n) {
+      double b0 = C[mk + n * LDC] * a0;
+      sb[mk * N + n] = C[mk + n * LDC] = b0;
+      for (BLASLONG m_up = 0; m_up < mk; ++m_up) {
+        C[m_up + n * LDC] -= b0 * sa[mk * M + m_up];
+      }
+    }
+  }
+}
+
+static inline void solve_lt_ref(const BLASLONG M, const BLASLONG N,
+  const double *sa, double *sb, double *C, BLASLONG LDC) {
+
+  for (BLASLONG mk = 0; mk < M; ++mk) {
+    double a0 = sa[mk * M + mk];
+    for (BLASLONG n = 0; n < N; ++n) {
+      double b0 = C[mk + n * LDC] * a0;
+      sb[mk * N + n] = C[mk + n * LDC] = b0;
+      for (BLASLONG m_lo = mk + 1; m_lo < M; ++m_lo) {
+        C[m_lo + n * LDC] -= b0 * sa[mk * M + m_lo];
+      }
+    }
+  }
+}
+
+static void solve_ln_reference(const BLASLONG M, BLASLONG N,
+  double *sa, double *sb, double *C, BLASLONG LDB, BLASLONG LDC) {
+
+  BLASLONG n = 4;
+  for (; N; N -= n) {
+    if (N < 4) n = N;
+    update_c(M, n, sb, C, LDC);
+    solve_ln_ref(M, n, sa, sb, C, LDC);
+    sb += n * LDB;
+    C += n * LDC;
+  }
+}
+
+static void solve_lt_reference(const BLASLONG M, BLASLONG N,
+  double *sa, double *sb, double *C, BLASLONG LDB, BLASLONG LDC) {
+
+  BLASLONG n = 4;
+  for (; N; N -= n) {
+    if (N < 4) n = N;
+    update_c(M, n, sb, C, LDC);
+    solve_lt_ref(M, n, sa, sb, C, LDC);
+    sb += n * LDB;
+    C += n * LDC;
+  }
+}
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+static inline double maxdiff(const double *a1, const double *a2, BLASLONG length) {
+  double max_diff = -1;
+  for (; length; length--) {
+    double diff = (*a1++) - (*a2++);
+    if (diff < 0) diff = -diff;
+    if (diff > max_diff) max_diff = diff;
+  }
+  return max_diff;
+}
+
+static inline void genrand(double *ar, BLASLONG length) {
+  srand(time(NULL));
+  for (; length; length--) (*ar++) = (double)rand() / RAND_MAX;
+}
+
+static inline void printmat(const double *src, size_t dim1, size_t dim2) {
+  for (; dim2; dim2--) {
+    for (size_t dim1_curr = dim1; dim1_curr; dim1_curr--) {
+      printf(" %.2e", *src++);
+    }
+    putchar('\n');
+  }
+  putchar('\n');
+}
+
+static void test_func(void (*testfunc)(double*, double*, double*, BLASLONG, BLASLONG),
+  void (*reffunc)(BLASLONG, BLASLONG, double*, double*, double*, BLASLONG, BLASLONG),
+  const BLASLONG M, const BLASLONG N) {
+
+  const int is_r = (reffunc == solve_rn_reference) || (reffunc == solve_rt_reference);
+  const BLASLONG K = is_r ? N : M;
+  const BLASLONG size_a = M * K;
+  const BLASLONG size_b = N * K;
+  const BLASLONG size_c = M * N;
+
+  double * const sa = (double*)malloc(size_a * sizeof(double));
+  double * const sb = (double*)malloc(size_b * sizeof(double));
+  double * const st = (double*)malloc(size_c * sizeof(double));
+  double * const C1 = (double*)malloc(size_c * sizeof(double));
+  double * const C2 = (double*)malloc(size_c * sizeof(double));
+
+  genrand(sa, size_a);
+  genrand(sb, size_b);
+  memcpy(st, is_r ? sa : sb, size_c * sizeof(double));
+
+  genrand(C1, size_c);
+  memcpy(C2, C1, size_c * sizeof(double));
+
+  (*reffunc)(M, N, sa, sb, C1, K, M);
+  (*testfunc)(is_r ? st : sa, is_r ? sb : st, C2, K, M);
+  printf("M = %ld, N = %ld, maxdiff_b = %.2e, maxdiff_c = %.2e", M, N,
+    maxdiff(is_r ? sa : sb, st, M * N), maxdiff(C1, C2, M * N));
+
+  if (N == 0) {
+    putchar('\n');
+    printmat(C1, M, N);
+    printmat(C2, M, N);
+    printmat(is_r ? sa : sb, N, M);
+    printmat(st, N, M);
+  }
+
+#if __linux__
+  memset(sa, 0, size_a * sizeof(double));
+  memset(sb, 0, size_b * sizeof(double));
+  memset(st, 0, size_c * sizeof(double));
+  struct timespec starttime, endtime;
+  const BLASLONG iters = 300000;
+  clock_gettime(CLOCK_MONOTONIC, &starttime);
+  for (BLASLONG i = 0; i < iters; ++i) {
+    (*testfunc)(is_r ? st : sa, is_r ? sb : st, C2, K, M);
+  }
+  clock_gettime(CLOCK_MONOTONIC, &endtime);
+  double nsec = 1.0e9 * (endtime.tv_sec - starttime.tv_sec) +
+    endtime.tv_nsec - starttime.tv_nsec;
+  printf(", GFLOPS = %.2e", (double)iters * M * K * N / nsec);
+  clock_gettime(CLOCK_MONOTONIC, &starttime);
+  for (BLASLONG i = 0; i < iters; ++i) (*reffunc)(M, N, sa, sb, C1, K, M);
+  clock_gettime(CLOCK_MONOTONIC, &endtime);
+  nsec = 1.0e9 * (endtime.tv_sec - starttime.tv_sec) +
+    endtime.tv_nsec - starttime.tv_nsec;
+  printf(", GFLOPS(ref) = %.2e", (double)iters * M * K * N / nsec);
+#endif
+  putchar('\n');
+
+  free(sa);
+  free(sb);
+  free(st);
+  free(C1);
+  free(C2);
+}
+
+int main() {
+  printf("test solve_ln\n");
+  test_func(dtrsm_solve_ln_m1n1, solve_ln_reference, 1, 1);
+  test_func(dtrsm_solve_ln_m2n1, solve_ln_reference, 2, 1);
+  test_func(dtrsm_solve_ln_m1n2, solve_ln_reference, 1, 2);
+  test_func(dtrsm_solve_ln_m2n2, solve_ln_reference, 2, 2);
+  test_func(dtrsm_solve_ln_m4n1, solve_ln_reference, 4, 1);
+  test_func(dtrsm_solve_ln_m8n1, solve_ln_reference, 8, 1);
+  test_func(dtrsm_solve_ln_m16n1, solve_ln_reference, 16, 1);
+  test_func(dtrsm_solve_ln_m4n2, solve_ln_reference, 4, 2);
+  test_func(dtrsm_solve_ln_m8n2, solve_ln_reference, 8, 2);
+  test_func(dtrsm_solve_ln_m16n2, solve_ln_reference, 16, 2);
+  test_func(dtrsm_solve_ln_m1n4, solve_ln_reference, 1, 4);
+  test_func(dtrsm_solve_ln_m1n8, solve_ln_reference, 1, 8);
+  test_func(dtrsm_solve_ln_m1n12, solve_ln_reference, 1, 12);
+  test_func(dtrsm_solve_ln_m2n4, solve_ln_reference, 2, 4);
+  test_func(dtrsm_solve_ln_m2n8, solve_ln_reference, 2, 8);
+  test_func(dtrsm_solve_ln_m2n12, solve_ln_reference, 2, 12);
+  test_func(dtrsm_solve_ln_m4n4, solve_ln_reference, 4, 4);
+  test_func(dtrsm_solve_ln_m4n8, solve_ln_reference, 4, 8);
+  test_func(dtrsm_solve_ln_m4n12, solve_ln_reference, 4, 12);
+  test_func(dtrsm_solve_ln_m8n4, solve_ln_reference, 8, 4);
+  test_func(dtrsm_solve_ln_m8n8, solve_ln_reference, 8, 8);
+  test_func(dtrsm_solve_ln_m8n12, solve_ln_reference, 8, 12);
+  test_func(dtrsm_solve_ln_m16n4, solve_ln_reference, 16, 4);
+  test_func(dtrsm_solve_ln_m16n8, solve_ln_reference, 16, 8);
+  test_func(dtrsm_solve_ln_m16n12, solve_ln_reference, 16, 12);
+  printf("test solve_lt\n");
+  test_func(dtrsm_solve_lt_m1n1, solve_lt_reference, 1, 1);
+  test_func(dtrsm_solve_lt_m2n1, solve_lt_reference, 2, 1);
+  test_func(dtrsm_solve_lt_m1n2, solve_lt_reference, 1, 2);
+  test_func(dtrsm_solve_lt_m2n2, solve_lt_reference, 2, 2);
+  test_func(dtrsm_solve_lt_m4n1, solve_lt_reference, 4, 1);
+  test_func(dtrsm_solve_lt_m8n1, solve_lt_reference, 8, 1);
+  test_func(dtrsm_solve_lt_m16n1, solve_lt_reference, 16, 1);
+  test_func(dtrsm_solve_lt_m4n2, solve_lt_reference, 4, 2);
+  test_func(dtrsm_solve_lt_m8n2, solve_lt_reference, 8, 2);
+  test_func(dtrsm_solve_lt_m16n2, solve_lt_reference, 16, 2);
+  test_func(dtrsm_solve_lt_m1n4, solve_lt_reference, 1, 4);
+  test_func(dtrsm_solve_lt_m1n8, solve_lt_reference, 1, 8);
+  test_func(dtrsm_solve_lt_m1n12, solve_lt_reference, 1, 12);
+  test_func(dtrsm_solve_lt_m2n4, solve_lt_reference, 2, 4);
+  test_func(dtrsm_solve_lt_m2n8, solve_lt_reference, 2, 8);
+  test_func(dtrsm_solve_lt_m2n12, solve_lt_reference, 2, 12);
+  test_func(dtrsm_solve_lt_m4n4, solve_lt_reference, 4, 4);
+  test_func(dtrsm_solve_lt_m4n8, solve_lt_reference, 4, 8);
+  test_func(dtrsm_solve_lt_m4n12, solve_lt_reference, 4, 12);
+  test_func(dtrsm_solve_lt_m8n4, solve_lt_reference, 8, 4);
+  test_func(dtrsm_solve_lt_m8n8, solve_lt_reference, 8, 8);
+  test_func(dtrsm_solve_lt_m8n12, solve_lt_reference, 8, 12);
+  test_func(dtrsm_solve_lt_m16n4, solve_lt_reference, 16, 4);
+  test_func(dtrsm_solve_lt_m16n8, solve_lt_reference, 16, 8);
+  test_func(dtrsm_solve_lt_m16n12, solve_lt_reference, 16, 12);
+  printf("test solve_rn\n");
+  test_func(dtrsm_solve_rn_m8n4, solve_rn_reference, 8, 4);
+  test_func(dtrsm_solve_rn_m8n8, solve_rn_reference, 8, 8);
+  test_func(dtrsm_solve_rn_m8n12, solve_rn_reference, 8, 12);
+  test_func(dtrsm_solve_rn_m16n4, solve_rn_reference, 16, 4);
+  test_func(dtrsm_solve_rn_m16n8, solve_rn_reference, 16, 8);
+  test_func(dtrsm_solve_rn_m16n12, solve_rn_reference, 16, 12);
+  printf("test solve_rt\n");
+  test_func(dtrsm_solve_rt_m8n4, solve_rt_reference, 8, 4);
+  test_func(dtrsm_solve_rt_m8n8, solve_rt_reference, 8, 8);
+  test_func(dtrsm_solve_rt_m8n12, solve_rt_reference, 8, 12);
+  test_func(dtrsm_solve_rt_m16n4, solve_rt_reference, 16, 4);
+  test_func(dtrsm_solve_rt_m16n8, solve_rt_reference, 16, 8);
+  test_func(dtrsm_solve_rt_m16n12, solve_rt_reference, 16, 12);
+  return 0;
+}
+
